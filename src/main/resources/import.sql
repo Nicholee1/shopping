66c496fc1ddbc4a2a2c391e2048187373ed3dd75
leech122 <2367570034@qq.com>
2022-10-07 10:13:30 +0800

    initializer

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO users VALUES (2147483641, true, '3200 West Road', 'customer1@email.com', 'customer1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', '123456789', 'ROLE_CUSTOMER');
+INSERT INTO users VALUES (2147483642, true, '2000 John Road', 'manager1@email.com', 'manager1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', '987654321', 'ROLE_MANAGER');
+INSERT INTO users VALUES (2147483643, true, '222 East Drive ', 'employee1@email.com', 'employee1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', '123123122', 'ROLE_EMPLOYEE');
+INSERT INTO users VALUES (2147483645, true, '3100 Western Road A', 'customer2@email.com', 'customer2', '$2a$10$0oho5eUbDqKrLH026A2YXuCGnpq07xJpuG/Qu.PYb1VCvi2VMXWNi', '2343456', 'ROLE_CUSTOMER');
+
+-- ----------------------------
+-- Records of cart
+-- ----------------------------
+INSERT INTO cart VALUES (2147483641);
+INSERT INTO cart VALUES (2147483642);
+INSERT INTO cart VALUES (2147483643);
+INSERT INTO cart VALUES (2147483645);
